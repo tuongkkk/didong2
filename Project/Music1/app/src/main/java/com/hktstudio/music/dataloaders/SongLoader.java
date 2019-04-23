182ac727a07b15dfc82d68a4ee78a23ea988463b
tuongkkk <36876719+tuongkkk@users.noreply.github.com>
2019-04-23 08:27:11 +0700

    Update SongLoader.java

--- Project/Music1/app/src/main/java/com/hktstudio/music/dataloaders/SongLoader.java
+++ Project/Music1/app/src/main/java/com/hktstudio/music/dataloaders/SongLoader.java
@@ -10,9 +10,7 @@ import com.hktstudio.music.models.Song;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by HOANG on 4/10/2018.
- */
+
 
 public class SongLoader {
     public static List getListSongs(Context context) {
